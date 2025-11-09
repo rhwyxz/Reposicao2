b415ebed5d99be447b543c5318f14ec4be949afe
kaua <kauaalexandrelopes@gmail.com>
2025-11-09 15:09:45 -0300

    reposicao2

---
+++ tabuada.py
@@ -0,0 +1,6 @@
+num = int(input("Digite um número inteiro: "))
+
+for i in range(1,11):
+    resultado = num * i
+    print(f"{num} x {i} = {resultado}")
+    
---
+++ verifcador.py
@@ -0,0 +1,8 @@
+name = input("Qual o seu nome ? ")
+idade = int(input("Digite sua idade ? "))
+
+if idade >= 18:
+    print(f"Olá {name}, você é maior de idade")
+
+else:
+    print(f"Olá {name}, você é menor de idade")    
